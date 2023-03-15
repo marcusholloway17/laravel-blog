7bf3f1665df973bcf1d4f306080071d35ffee1a3
marcusholloway17 <loganjean17@gmail.com>
2023-03-15 12:09:48 +0000

    update

---
+++ resources/views/dashboard/post/create.blade.php
@@ -0,0 +1,74 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Publier un article') }}
+        </h2>
+
+        @if (session('success'))
+            <div class="bg-teal-100 border-l-4 border-teal-500 text-teal-900 p-4 shadow-md bg-white dark:bg-gray-800 dark:text-gray-100 mt-5"
+                role="alert" x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)">
+                <p class="font-bold">Succès</p>
+                <p class="text-sm">{{ session('success') }}</p>
+            </div>
+        @endif
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <section>
+                    <header>
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                            {{ __('Formulaire') }}
+                        </h2>
+
+                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                            {{ __('Remplissez ce formulaire pour publier un nouvel article') }}
+                        </p>
+                    </header>
+                </section>
+
+                <section style="margin-top: 2rem">
+                    <form action="{{ route('posts.store') }}" method="POST" class="mt-6 space-y-6">
+                        @csrf
+                        @method('post')
+
+                        <div>
+                            <x-input-label for="title" :value="__('Titre')" />
+                            <x-text-input id="title" name="title" type="text" class="mt-1 block w-full"
+                                required autofocus autocomplete="title" />
+                            <x-input-error class="mt-2" :messages="$errors->get('title')" />
+                        </div>
+
+                        <div>
+                            <x-input-label for="content" :value="__('Contenu')" />
+                            <textarea id="summernote" name="content" class="mt-1 block w-full" required></textarea>
+                            <x-input-error class="mt-2" :messages="$errors->get('content')" />
+                        </div>
+
+                        <div>
+                            <x-input-label for="category" :value="__('Categorie')" />
+                            <select name="category" id="category"
+                                class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded leading-tight focus:outline-none focus:shadow-outline">
+                                @foreach ($categories as $category)
+                                    <option value="{{ $category->id }}"> {{ $category->label }}</option>
+                                @endforeach
+                            </select>
+                            <x-input-error class="mt-2" :messages="$errors->get('category')" />
+                        </div>
+
+                        <div>
+                            <x-input-label for="image" value="Image mise en avant"></x-input-label>
+                            <x-text-input id="image" name="image" type=file required></x-text-input>
+                            <x-input-error class="mt-2" :messages="$errors->get('image')" />
+                        </div>
+
+                        <div class="flex items-center gap-4">
+                            <x-primary-button>{{ __('Soumettre') }}</x-primary-button>
+                        </div>
+                    </form>
+                </section>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/dashboard/post/index.blade.php
@@ -0,0 +1,109 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Publications') }}
+        </h2>
+
+        @if (session('success'))
+            <div class="bg-teal-100 border-l-4 border-teal-500 text-teal-900 p-4 shadow-md bg-white dark:bg-gray-800 dark:text-gray-100 mt-5"
+                role="alert" x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)">
+                <p class="font-bold">Succès</p>
+                <p class="text-sm">{{ session('success') }}</p>
+            </div>
+        @endif
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <section>
+                    <div class="flex justify-between">
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                {{ __('Liste d\'article') }}
+                            </h2>
+
+                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                {{ __('Menu de gestion des articles publiés') }}
+                            </p>
+                        </header>
+
+                        <button
+                            class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out">
+                            <a href="{{ route('posts.create') }}">Rédiger</a>
+                        </button>
+                    </div>
+                </section>
+
+                <section style="margin-top: 2rem;">
+                    <div class="px-3 py-4 ">
+                        <table class="min-w-max w-full table-auto">
+                            <thead class="bg-gray-100 dark:bg-gray-700">
+                                <tr>
+                                    <th scope="col"
+                                        class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                        {{ __('Titre') }}
+                                    </th>
+                                    <th scope="col"
+                                        class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                        {{ __('Contenu') }}
+                                    </th>
+                                    <th scope="col"
+                                        class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                        {{ __('Categorie') }}
+                                    </th>
+                                    <th scope="col"
+                                        class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                        {{ __('Auteur') }}
+                                    </th>
+                                    <th scope="col"
+                                        class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                        {{ __('Publié le') }}
+                                    </th>
+                                    <th scope="col" class="p-4">
+                                        <span class="sr-only">Actions</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                @foreach ($posts as $post)
+                                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                        <td
+                                            class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $post->title }}</td>
+                                        <td
+                                            class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                                            {{ Str::limit($post->content, 300, '...') }}</td>
+                                        <td
+                                            class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $post->category->label }}
+                                        </td>
+                                        <td
+                                            class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ $post->user->name }}
+                                        </td>
+                                        <td class="py-4 px-6 text-sm font-medium text-right whitespace-nowrap">
+                                            <a href="{{ route('posts.edit', $post) }}"
+                                                class="text-blue-600 dark:text-blue-500 hover:underline dark:text-white"
+                                                style="display: block">Editer</a>
+
+                                            <form action="{{ route('posts.destroy', $post) }}" method="post"
+                                                style="display: block">
+                                                @csrf
+                                                @method('delete')
+                                                <button
+                                                    class="text-red-600 dark:text-blue-500 hover:underline dark:text-red align-items-md-end">
+                                                    Supprimer
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
